cf1d40d51ad7aabd02444e94471e888db6ed6a94
miquelven <miquelven.silva@gmail.com>
2024-02-09 16:36:57 -0300

    adição page about

--- pages/About.vue
+++ pages/About.vue
@@ -1,5 +1,46 @@
 <template>
-  <div>Página about</div>
+  <section id="about" data-aos="zoom-in">
+    <h2 data-aos="fade-up" data-aos-delay="300">
+      <span>Explore</span> and <span>Celebrate</span> Beverage Culture
+    </h2>
+    <p>
+      Welcome to Midrinks, where we delve deep into the fascinating world of
+      beverages to bring you a unique experience of discovery and celebration.
+    </p>
+    <p>
+      At Midrinks, it's not just about selling beverages – our aim is to provide
+      a platform where you can explore and appreciate the richness of beverage
+      culture. We don't sell drinks; we tell their stories.
+    </p>
+    <p>
+      Explore our extensive database, packed with detailed information on a
+      variety of beverages, from the most iconic to the most obscure. Each entry
+      is carefully curated and presented with a wealth of details, from photos
+      and aromatic descriptions to ingredients and preparation methods.
+    </p>
+    <p>
+      Whether you're interested in fine wines, craft beers, rare spirits, or
+      non-alcoholic beverages, we have something for every palate. Our team of
+      experts is constantly updating our catalog to ensure you have access to
+      the latest and most accurate information.
+    </p>
+    <p>
+      Furthermore, we are committed to promoting responsible and sustainable
+      practices in the beverage industry, highlighting producers who share our
+      values of quality, authenticity, and environmental stewardship.
+    </p>
+    <p>
+      Join us on this journey of exploration and learning. Discover new
+      beverages, learn about their origins and ingredients, and immerse yourself
+      in the rich tapestry of beverage culture from around the world.
+    </p>
+    <p>
+      At Midrinks, we celebrate the diversity, creativity, and passion that make
+      the world of beverages truly extraordinary.
+    </p>
+    <p>Appreciatively,</p>
+    <span data-aos="fade-up" data-aos-delay="600">The Midrinks Team</span>
+  </section>
 </template>
 
 <script setup></script>

902b745afd1ecedd48234e60a02e1d72e21a6285
miquelven <miquelven.silva@gmail.com>
2024-02-09 17:07:32 -0300

    remoção do nuxtjs/color-mode

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,22 +11,5 @@ export default defineNuxtConfig({
       },
     },
   },
-  modules: [
-    "nuxt-icon",
-    "@nuxt/ui",
-    "@vueuse/nuxt",
-    "nuxt-aos",
-    "@pinia/nuxt",
-    "@nuxtjs/color-mode",
-  ],
-  colorMode: {
-    preference: "system", // default value of $colorMode.preference
-    fallback: "dark", // fallback value if not system preference found
-    hid: "nuxt-color-mode-script",
-    globalName: "__NUXT_COLOR_MODE__",
-    componentName: "ColorScheme",
-    classPrefix: "",
-    classSuffix: "-mode",
-    storageKey: "nuxt-color-mode",
-  },
+  modules: ["nuxt-icon", "@nuxt/ui", "@vueuse/nuxt", "nuxt-aos", "@pinia/nuxt"],
 });

870a50f7efc60513490b31adf777671d2d4b003a
miquelven <miquelven.silva@gmail.com>
2024-02-08 22:27:17 -0300

    scroll behavior

--- components/DrinkFilterList/index.vue
+++ components/DrinkFilterList/index.vue
@@ -1,6 +1,6 @@
 <template>
   <ul id="drinkFiltered">
-    <li v-for="(drink, index) in drinkValue" :key="index">
+    <li v-for="(drink, index) in drinkValue" :key="index" data-aos="zoom-in">
       <div class="imgArea">
         <img :src="drink.img" alt="Imagem de uma bebida" />
         <div>
@@ -11,33 +11,35 @@
     </li>
   </ul>
 
-  <UPagination
-    v-model="page"
-    :total="drink"
-    :ui="{ rounded: 'first-of-type:rounded-s-md last-of-type:rounded-e-md' }"
-    :active-button="{ color: 'purple' }"
-    @click="update"
-  >
-    <UTooltip text="Previous page">
-      <UButton
-        icon="i-heroicons-arrow-small-left-20-solid"
-        color="purple"
-        :ui="{ rounded: 'rounded-full' }"
-        class="rtl:[&_span:first-child]:rotate-180 me-2"
-        @click="update"
-      />
-    </UTooltip>
+  <NuxtLink to="#drinkFilter" @click="updateScroll">
+    <UPagination
+      v-model="page"
+      :total="drink"
+      :ui="{ rounded: 'first-of-type:rounded-s-md last-of-type:rounded-e-md' }"
+      :active-button="{ color: 'purple' }"
+      @click="update"
+    >
+      <UTooltip text="Previous page">
+        <UButton
+          icon="i-heroicons-arrow-small-left-20-solid"
+          color="purple"
+          :ui="{ rounded: 'rounded-full' }"
+          class="rtl:[&_span:first-child]:rotate-180 me-2"
+          @click="update"
+        />
+      </UTooltip>
 
-    <UTooltip text="Next page">
-      <UButton
-        icon="i-heroicons-arrow-small-right-20-solid"
-        color="purple"
-        :ui="{ rounded: 'rounded-full' }"
-        class="rtl:[&_span:last-child]:rotate-180 ms-2"
-        @click="update"
-      />
-    </UTooltip>
-  </UPagination>
+      <UTooltip text="Next page">
+        <UButton
+          icon="i-heroicons-arrow-small-right-20-solid"
+          color="purple"
+          :ui="{ rounded: 'rounded-full' }"
+          class="rtl:[&_span:last-child]:rotate-180 ms-2"
+          @click="update"
+        />
+      </UTooltip>
+    </UPagination>
+  </NuxtLink>
 </template>
 
 <script setup>
@@ -72,6 +74,11 @@ const drink = computed(() => {
 });
 
 const router = useRouter();
+const route = useRoute();
+
+const updateScroll = () => {
+  console.log(route);
+};
 
 const redirectShowMore = (id) => {
   router.push(`drinksInfo/${id}`);

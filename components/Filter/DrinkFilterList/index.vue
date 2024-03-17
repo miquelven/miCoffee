b4ec827913097aa452f6385a835777350230dc58
miquelven <miquelven.silva@gmail.com>
2024-03-17 14:42:49 -0300

    mudança no layout

--- components/Filter/DrinkFilterList/index.vue
+++ components/Filter/DrinkFilterList/index.vue
@@ -12,7 +12,33 @@
   </ul>
 
   <NuxtLink to="#drinkFilter" @click="updateScroll">
-    <!-- Seu código de paginação -->
+    <UPagination
+      v-model="page"
+      :total="drink"
+      :ui="{ rounded: 'first-of-type:rounded-s-md last-of-type:rounded-e-md' }"
+      :active-button="{ color: 'black' }"
+      @click="update"
+    >
+      <UTooltip text="Previous page">
+        <UButton
+          icon="i-heroicons-arrow-small-left-20-solid"
+          color="purple"
+          :ui="{ rounded: 'rounded-full' }"
+          class="rtl:[&_span:first-child]:rotate-180 me-2"
+          @click="update"
+        />
+      </UTooltip>
+
+      <UTooltip text="Next page">
+        <UButton
+          icon="i-heroicons-arrow-small-right-20-solid"
+          color="purple"
+          :ui="{ rounded: 'rounded-full' }"
+          class="rtl:[&_span:last-child]:rotate-180 ms-2"
+          @click="update"
+        />
+      </UTooltip>
+    </UPagination>
   </NuxtLink>
 </template>
 

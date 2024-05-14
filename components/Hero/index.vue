6045a6e7299a8b982911be4d911456c77e6ddd71
miquelven <miquelven.silva@gmail.com>
2024-05-14 08:39:09 -0300

    style: remove transitions and add in hero section

--- components/Hero/index.vue
+++ components/Hero/index.vue
@@ -1,18 +1,23 @@
 <template>
-  <section id="hero">
+  <section
+    id="hero"
+    data-aos="zoom-in"
+    data-aos-delay="300"
+    data-aos-duration="500"
+  >
     <div id="heroContainer">
-      <h1 data-aos="fade-right" data-aos-delay="300">
+      <h1>
         <span class="purple-text-primary">Unveiling</span> the World of
         Beverages: A Glass of
         <span class="purple-text-primary">Stories</span> and
         <span class="purple-text">Aromas</span>
       </h1>
-      <p data-aos="fade-right" data-aos-delay="500">
+      <p>
         This is the place where you find many
         <span class="purple-text-secondary"> information</span> about all
         <span class="purple-text-secondary">type of beverages</span>
       </p>
-      <p data-aos="fade-right" data-aos-delay="700">
+      <p>
         <span class="purple-text-secondary">Explore</span>
         and <span class="purple-text-secondary"> get to know more</span> about
         each one of them
